77dca321d6575b47ae3f75f81083c18f20475e15
cruth <ceruth@epfc.eu>
2021-03-07 21:34:54 +0100

    Relation ManyToMany between Artist & Type

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -15,4 +15,12 @@ class Type extends Model
 
     public $timestamps = false;
 
+    /**
+     * The artists that are defined by the type.
+     */
+    public function artists()
+    {
+        return $this->belongsToMany(Artist::class);
+    }
+
 }
